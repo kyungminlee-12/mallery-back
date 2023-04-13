3e46fea45f6e32716bd0217de3ea66cd8c7c35a3
Kyungmin Lee <min2000.lee@gmail.com>
2023-04-13 18:34:33 +0900

    [feat] #7 idCheck permitAll()

--- src/main/java/com/example/graduation4/config/SecurityConfig.java
+++ src/main/java/com/example/graduation4/config/SecurityConfig.java
@@ -69,16 +69,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 //URL 관리
                 .authorizeRequests()
-                .antMatchers("/member/login", "/swagger-ui/**", "/member/signup").permitAll()
-                .antMatchers("/member/logout", "/member/{userId}").hasRole("USER")
-                // .antMatchers("/member/admin", "/member/{userId}").hasRole("ROLE_ADMIN")
                 .anyRequest().authenticated()
+                .antMatchers("/member/login", "/swagger-ui/**", "/member/signup", "member/idCheck", "member/reissue").permitAll()
+                // .antMatchers("/member/logout", "/member/{userId}").hasRole("USER")
+                // .antMatchers("/member/admin", "/member/{userId}").hasRole("ROLE_ADMIN")
                 //.invalidateHttpSession(true);
 
                 .and()
                 // JwtAuthenticationFilter를 먼저 적용
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .httpBasic();
+
+        http.formLogin()
+                .loginPage("/member/login");
     }
 
     /*
