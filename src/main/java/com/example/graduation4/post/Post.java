99bd8243c3701263bdb4747bc0fd349e14db604e
Kyungmin Lee <min2000.lee@gmail.com>
2023-05-01 12:53:19 +0900

    feat/#13/생성, 친구추가

---
+++ src/main/java/com/example/graduation4/post/Post.java
@@ -0,0 +1,54 @@
+package com.example.graduation4.post;
+
+import com.example.graduation4.BaseEntity;
+import com.example.graduation4.group.Album;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "post")
+public class Post extends BaseEntity  {
+
+    @Id                                                  // Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
+    @Column(name="post_id")
+    private Long postId;
+
+    // @Column
+    // private String postLocation;
+
+    // 자신의 위치 정보 입력
+    @Column
+    private String postLocation;
+
+    @Column
+    private String postDate;
+
+    // 이미지 경로
+    // @NotEmpty
+    // private String storedFilePath;
+
+    @ManyToOne
+    @JoinColumn(name = "album_id")
+    private Album album;
+
+    @OneToMany(mappedBy = "post")
+    private List<Participant> participants = new ArrayList<>();
+
+
+    // @Column
+    // private int memberCnt;
+
+    // @OneToMany(mappedBy = "album")
+    // private List<Room> rooms = new ArrayList<>();
+
+}
