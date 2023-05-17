e517a45b5321344cff344d60690759d348ef931f
Kyungmin Lee <min2000.lee@gmail.com>
2023-05-18 03:47:31 +0900

    [fix] post userId, nickname 별도 list로 보내기

--- src/main/java/com/example/graduation4/post/dto/PostResponseDto.java
+++ src/main/java/com/example/graduation4/post/dto/PostResponseDto.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -31,13 +32,21 @@ public class PostResponseDto {
         private String postLocation;
         private String userId;
         private int memberCnt;
-        private List<MemberRes> members;
+        private List<String> members;
+        private List<String> nicknames;
         private List<String> imagePaths;
     }
 
     public ResponseEntity<?> postSuccess(Post post1) {
 
         List<MemberRes> members_list = postRepository.findAllMembersByPostId(post1.getPostId());
+        List<String> members_li = new ArrayList<>();
+        List<String> nicknames_li = new ArrayList<>();
+
+        for (int i=0 ; i < members_list.size() ; i++) {
+            members_li.add(members_list.get(i).getUserId());
+            nicknames_li.add(members_list.get(i).getUsername());
+        }
 
         PostBody body = PostBody.builder()
                 .state(200)
@@ -47,7 +56,8 @@ public class PostResponseDto {
                 .userId(members_list.get(0).getUserId())
                 .memberCnt(members_list.size())
                 .imagePaths(post1.getImagePaths())
-                .members(members_list)
+                .members(members_li)
+                .nicknames(nicknames_li)
                 .build();
 
         return ResponseEntity.ok(body);
